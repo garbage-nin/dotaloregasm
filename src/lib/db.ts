6946e59d47dbc8882476de19e5d4c333473c62b0
kj_dv <garbage.nin@gmail.com>
2025-04-21 04:53:00 +0800

    adjust database url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,10 @@ import env from "dotenv";
 
 env.config();
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT ?? "5432"),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true, // for self-signed certs, otherwise set to true
+  },
 });
 
 export default pool;
