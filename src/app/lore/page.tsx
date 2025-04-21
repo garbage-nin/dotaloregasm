cd0e11fc4d21a4741fae41630bf0d291fe883740
kj_dv <garbage.nin@gmail.com>
2025-04-21 15:20:41 +0800

    lefout commit

--- src/app/lore/page.tsx
+++ src/app/lore/page.tsx
@@ -10,7 +10,10 @@ export default async function LorePage() {
   const heroeList = heroesResp.map((hero: any) => ({
     id: hero.id,
     label: hero.name_loc,
-    image: `/images/${hero.name.replace("npc_dota_hero_", "")}.png`,
+    image: `${process.env.CDN_URL}/${hero.name.replace(
+      "npc_dota_hero_",
+      ""
+    )}.png`,
   }));
 
   return (
