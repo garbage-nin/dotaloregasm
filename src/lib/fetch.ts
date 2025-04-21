b3c54b38c5954318b697e2a6c719ca5213a5aa6a
kj_dv <garbage.nin@gmail.com>
2025-04-21 14:03:26 +0800

    made the api to be an env variable

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -4,14 +4,16 @@ export async function fetchApi<T>(
   method: "GET" | "POST" | "PUT" = "GET",
   body?: any
 ): Promise<T> {
-  let apiUrl = `https://www.dotaloregasm.com/api/${endpoint}`;
+  let apiUrl = `${process.env.NEXT_PUBLIC_URL}/api/${endpoint}`;
 
   if (Object.keys(parameters).length > 0 && parameters.returnFields) {
     const queryParams = new URLSearchParams();
     parameters.returnFields.forEach((field: string) =>
       queryParams.append("returnFields", field)
     );
-    apiUrl = `https://www.dotaloregasm.com/api/${endpoint}?${queryParams.toString()}`;
+    apiUrl = `${
+      process.env.NEXT_PUBLIC_URL
+    }/api/${endpoint}?${queryParams.toString()}`;
   }
 
   const res = await fetch(apiUrl, {
