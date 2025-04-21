93bd433d50f014c4e7c601b90e5bad942b740565
kj_dv <garbage.nin@gmail.com>
2025-04-21 14:14:20 +0800

    env variable changes

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -4,18 +4,15 @@ export async function fetchApi<T>(
   method: "GET" | "POST" | "PUT" = "GET",
   body?: any
 ): Promise<T> {
-  let apiUrl = `${process.env.NEXT_PUBLIC_URL}/api/${endpoint}`;
+  let apiUrl = `${process.env.DOMAIN_URL}/api/${endpoint}`;
 
-  console.log("NEXT_PUBLIC_URL", process.env.NEXT_PUBLIC_URL);
-  console.log("DOMAIN_URL", process.env.DOMAIN_URL);
-  console.log("API_KEY", process.env.API_KEY);
   if (Object.keys(parameters).length > 0 && parameters.returnFields) {
     const queryParams = new URLSearchParams();
     parameters.returnFields.forEach((field: string) =>
       queryParams.append("returnFields", field)
     );
     apiUrl = `${
-      process.env.NEXT_PUBLIC_URL
+      process.env.DOMAIN_URL
     }/api/${endpoint}?${queryParams.toString()}`;
   }
 
